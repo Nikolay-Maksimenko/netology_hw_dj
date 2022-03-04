48d18f702ed072fcb7c4eca469d064914775c9ad
Nikolay Maksimenko <horrendos@mail.ru>
2022-03-04 23:26:35 +0300

    correcting hw testing

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,19 +1,20 @@
 import pytest
 from django.urls import reverse
+import random
 from students.models import Course
 from tests.conftest import course_factory, student_factory
 
 @pytest.mark.django_db
 def test_course_retrieve(client, course_factory):
-    course_factory(_quantity=10)
-    course = Course.objects.get(pk=7)
-    url = reverse('courses-detail', args=(course.id,))
+    courses = course_factory(_quantity=10)
+    course_id = random.randint(0, len(courses)-1)
+    url = reverse('courses-detail', args=(courses[course_id].id,))
     response = client.get(url)
     response_json = response.json()
     print(response_json)
     assert response.status_code == 200
-    assert response_json['id'] == course.id
-    assert response_json['name'] == course.name
+    assert response_json['id'] == courses[course_id].id
+    assert response_json['name'] == courses[course_id].name
 
 @pytest.mark.django_db
 def test_course_list(client, course_factory):
@@ -26,22 +27,22 @@ def test_course_list(client, course_factory):
 
 @pytest.mark.django_db
 def test_course_filter_id(client, course_factory):
-    course_factory(_quantity=10)
-    course = Course.objects.first()
-    url = reverse("courses-list") + f'?id={course.id}'
+    courses = course_factory(_quantity=10)
+    course_id = random.randint(0, len(courses) - 1)
+    url = reverse("courses-list") + f'?id={courses[course_id].id}'
     response = client.get(url)
     response_json = response.json()
     assert response.status_code == 200
-    assert response_json[0].get('id') == course.id
+    assert response_json[0].get('id') == courses[course_id].id
 
 @pytest.mark.django_db
 def test_course_filter_name(client, course_factory):
-    course_factory(_quantity=10)
-    course = Course.objects.first()
-    url = reverse("courses-list") + f'?name={course.name}'
+    courses = course_factory(_quantity=10)
+    course_id = random.randint(0, len(courses) - 1)
+    url = reverse("courses-list") + f'?name={courses[course_id].name}'
     response = client.get(url)
     assert response.status_code == 200
-    assert response.data[0].get('name') == course.name
+    assert response.data[0].get('name') == courses[course_id].name
 
 @pytest.mark.django_db
 def test_course_create(client, student_factory):
@@ -54,18 +55,18 @@ def test_course_create(client, student_factory):
 
 @pytest.mark.django_db
 def test_course_update(client, course_factory):
-    course_factory(_quantity=5)
-    course_update = Course.objects.first()
-    url = reverse("courses-detail", args=(course_update.id,))
+    courses = course_factory(_quantity=5)
+    course_id = random.randint(0, len(courses) - 1)
+    url = reverse("courses-detail", args=(courses[course_id].id,))
     update_data = {'name': 'ruby'}
     response = client.patch(url, update_data)
     assert response.status_code == 200
 
 @pytest.mark.django_db
 def test_course_delete(client, course_factory):
-    course_factory(_quantity=5)
-    course_del = Course.objects.first()
-    url = reverse("courses-detail", args=(course_del.id,))
+    courses = course_factory(_quantity=5)
+    course_id = random.randint(0, len(courses) - 1)
+    url = reverse("courses-detail", args=(courses[course_id].id,))
     data_del = {'name': 'ruby'}
     response = client.delete(url, data_del)
     assert response.status_code == 204
